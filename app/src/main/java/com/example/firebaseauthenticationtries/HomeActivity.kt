2c5c5a008b41c96103ca2d4d83aaa10dfef0d084
Harsh Panchal <harshhhh1803@gmail.com>
2023-01-07 14:16:45 +0530

    Completed entire authentication model

--- app/src/main/java/com/example/firebaseauthenticationtries/HomeActivity.kt
+++ app/src/main/java/com/example/firebaseauthenticationtries/HomeActivity.kt
@@ -3,16 +3,16 @@ package com.example.firebaseauthenticationtries
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.activity_home.editPass
-import kotlinx.android.synthetic.main.activity_home.tinput_pass
-import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var auth:FirebaseAuth
+    private lateinit var user: FirebaseUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,30 +20,57 @@ class HomeActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
         auth=FirebaseAuth.getInstance()
+        user= auth.currentUser!!
+
+        loggeduser.text=user.email
 
         signoutbtn.setOnClickListener {
+            auth.signOut()
             startActivity(Intent(this,SignInActivity::class.java))
             finish()
         }
 
         update_pass_btn.setOnClickListener {
-            val user=auth.currentUser
             val password=editPass.text.toString()
-            if(checkFields()){
-                if(user!=null){
-                    user.updatePassword(password).addOnCompleteListener {
-                        if(it.isSuccessful){
-                            Toast.makeText(this,"Password updated successfully",Toast.LENGTH_SHORT).show()
-                        } else {
-                            Toast.makeText(this,it.exception?.message,Toast.LENGTH_SHORT).show()
-                        }
+            if(checkPasswordFields()){
+                user.updatePassword(password).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        Toast.makeText(this,"Password updated successfully",Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this,it.exception?.message,Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
 
+        update_mail_btn.setOnClickListener {
+            val mail=editMail.text.toString()
+            if(checkMailFields()){
+                user.updateEmail(mail).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        Toast.makeText(this,"Mail Updated Successfully",Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this,SignInActivity::class.java))
+                    } else {
+                        Toast.makeText(this,it.exception?.message,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        delete_acc_btn.setOnClickListener {
+            user.delete().addOnCompleteListener {
+                if(it.isSuccessful){
+                    Toast.makeText(this,"You're Kicked out of this",Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,SignInActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(this,it.exception?.message,Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
-    private fun checkFields():Boolean{
+
+    private fun checkPasswordFields():Boolean{
         if(editPass.text.toString()== ""){
             tinput_pass.error="Required field"
             tinput_pass.errorIconDrawable=null
@@ -54,6 +81,19 @@ class HomeActivity : AppCompatActivity() {
             tinput_pass.error="Password should at least 8 digits long"
             tinput_pass.errorIconDrawable=null
                 return false
+        }
+        return true
+    }
+
+    private fun checkMailFields():Boolean{
+        if(editMail.text.toString()== ""){
+            tinput_mail.error="Required field"
+            return false
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(editMail.text.toString()).matches()){
+            tinput_mail.error="Invalid mail format"
+            return false
         }
         return true
     }
--- app/src/main/java/com/example/firebaseauthenticationtries/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauthenticationtries/MainActivity.kt
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+        auth=FirebaseAuth.getInstance()
+        val user =auth.currentUser
 
         // to put delay
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this,SignInActivity::class.java))
+            if(user!=null)
+                startActivity(Intent(this,HomeActivity::class.java))
+            else
+                startActivity(Intent(this,SignInActivity::class.java))
             finish()
         },3000)
     }
